03aecb8e2911d7ef35c1b6ff9b4338b370f08c04
gn-pmcgeough <pierce.mcgeough@nbcuni.com>
2023-07-25 21:51:14 +0100

    Move to includes

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-    require_once '_functions.php';
+    require_once './includes/_functions.php';
     $clubs = getClubs();
 ?>
 <!DOCTYPE html>
@@ -11,18 +11,18 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    <?php include '_header.php'; ?>
+    <?php include './includes/_header.php'; ?>
 
     <div class="py-5 bg-body-tertiary">
         <div class="container">
             <?php if (isset($clubs) && (count($clubs) > 0)): ?>
                 <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
                     <?php foreach ($clubs as $club): ?>
-                        <?php include '_club_item.php'; ?>
+                        <?php include './includes/_club_item.php'; ?>
                     <?php endforeach; ?>
                 </div>
             <?php else: ?>
-                <?php include '_no_clubs_found.php'; ?>
+                <?php include './includes/_no_clubs_found.php'; ?>
             <?php endif; ?>
         </div>
     </div>
